12be1b4541a943c3d1cfb812e9754d64f72f7c00
angrypig555 <84266804+angrypig555@users.noreply.github.com>
2025-12-29 17:05:36 +0000

    update

--- rss-term.cpp
+++ rss-term.cpp
@@ -1,14 +1,1 @@
-﻿#include<iostream>
-#include<curlpp/cURLpp.hpp>
-#include<curlpp/Options.hpp>
-
-int main() {
-	curlpp::Cleanup myCleanup
-
-	std::ostringstream os;
-	os << curlpp::options::Url(std::string("http://modernhackers.com"));
-
-	string mystring = os.str();
-
-	return 0;
-}
+﻿// todo write code
